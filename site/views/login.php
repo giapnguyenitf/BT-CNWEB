dbe205dca07928c760e51398e94faacd165726d8
GNPC\giapn <giapnguyenitf@gmail.com>
2017-05-11 22:19:16 +0700

    folder

---
+++ site/views/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h4 style="text-align:center">LOGIN</h4>
+            </div>
+            <div class="panel-body">
+              <form class="form-group" action="#" method="post">
+                <input class="form-control" type="text" name="username" value="" placeholder="Username..">
+                <input class="form-control" type="password" name="password" value="" placeholder="Password..">
+                <input type="checkbox" name="remember_pass" value=""> Nhớ mật khẩu?
+              </form>
+
+            </div>
+            <div class="panel-footer">
+              <p>Bạn chưa có tài khoản? <a href="#">Register</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="../../public/js/jquery-3.2.1.min.js"></script>
+    <script src="../../public/js/bootstrap.min.js"></script>
+  </body>
+</html>
